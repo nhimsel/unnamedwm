6fd0d9ae5ca910fd40b1fd28373c5dae05411901
UnleashedFurai <67919489+UnleashedFurai@users.noreply.github.com>
2026-06-25 12:17:26 -0500

    some changes

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@ OBJS = $(SRCS:.c=.o)
 
 TARGET = unnamed
 CC = clang
-CFLAGS = -std=c99 -pedantic -Wall -Werror
+CFLAGS = -std=c99 -pedantic -Wall -Werror -O2
 LDFLAGS = -lX11
 
 all: $(TARGET)
+
+debug: CFLAGS += -g -O0 -DDEBUG
+debug: $(TARGET)
 
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) $(LDFLAGS) -o $(TARGET)
--- unnamed.c
+++ unnamed.c
@@ -34,6 +34,54 @@ int otherwmerr(Display *d, XErrorEvent *e)
 	exit(1);
 }
 
+#ifdef DEBUG
+const char* namexevent(int type)
+{
+    switch (type) {
+    case KeyPress:        return "KeyPress";
+    case KeyRelease:      return "KeyRelease";
+    case ButtonPress:     return "ButtonPress";
+    case ButtonRelease:   return "ButtonRelease";
+    case MotionNotify:    return "MotionNotify";
+    case EnterNotify:     return "EnterNotify";
+    case LeaveNotify:     return "LeaveNotify";
+    case FocusIn:         return "FocusIn";
+    case FocusOut:        return "FocusOut";
+	case KeymapNotify:    return "KeymapNotify";
+    case Expose:          return "Expose";
+	case GraphicsExpose:  return "GraphicsExpose";
+	case NoExpose:        return "NoExpose";
+	case VisibilityNotify:return "VisibilityNotify";
+	case CreateNotify:    return "CreateNotify";
+	case DestroyNotify:   return "DestroyNotify";
+    case UnmapNotify:     return "UnmapNotify";
+    case MapNotify:       return "MapNotify";
+	case MapRequest:      return "MapRequest";
+	case ReparentNotify:  return "ReparentNotify";
+	case ConfigureNotify: return "ConfigureNotify";
+	case ConfigureRequest:return "ConfigureRequest";
+	case GravityNotify:   return "GravityNotify";
+	case ResizeRequest:   return "ResizeRequest";
+	case CirculateNotify: return "CirculateNotify";
+	case CirculateRequest:return "CirculateRequest";
+	case PropertyNotify:  return "PropertyNotify";
+	case SelectionClear:  return "SelectionClear";
+	case SelectionRequest:return "SelectionRequest";
+	case SelectionNotify: return "SelectionNotify";
+	case ColormapNotify:  return "ColormapNotify";
+    case ClientMessage:   return "ClientMessage";
+	case MappingNotify:   return "MappingNotify";
+	case GenericEvent:    return "GenericEvent";
+	case LASTEvent:        return "LASTEvent";
+    default: {
+		static char buf[256];
+		snprintf(buf, sizeof(buf), "Unknown: %d", type);
+		return buf;	
+	}
+    }
+}
+#endif
+
 void checkwm(void)
 {
 	XSetErrorHandler(otherwmerr);
@@ -57,14 +105,16 @@ void keyhook(void)
 		XGrabKey(dis, XKeysymToKeycode(dis, XStringToKeysym("r")),
 				 Mod1Mask | nullmod[i], DefaultRootWindow(dis), True,
 				 GrabModeAsync, GrabModeAsync);
-	}
 
-	XGrabButton(dis, 1, Mod1Mask, DefaultRootWindow(dis), True,
-				ButtonPressMask|ButtonReleaseMask|PointerMotionMask,
-				GrabModeAsync, GrabModeAsync, None, None);
-	XGrabButton(dis, 3, Mod1Mask, DefaultRootWindow(dis), True,
-				ButtonPressMask|ButtonReleaseMask|PointerMotionMask,
-				GrabModeAsync, GrabModeAsync, None, None);
+		// hook alt-mouse1
+		XGrabButton(dis, 1, Mod1Mask | nullmod[i], DefaultRootWindow(dis), True,
+					ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+					GrabModeAsync, GrabModeAsync, None, None);
+		// hook alt-mouse3
+		XGrabButton(dis, 3, Mod1Mask | nullmod[i], DefaultRootWindow(dis), True,
+					ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+					GrabModeAsync, GrabModeAsync, None, None);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -95,40 +145,12 @@ int main(int argc, char *argv[])
 	{
 		XNextEvent(dis, &e);
 		switch (e.type) {
-		case KeyPress: {
-			KeySym k = XLookupKeysym(&e.xkey, 0);
-
-			switch (k) {
-			case XK_r:
-				if (e.xkey.state & Mod1Mask) exec("rofi -show drun");
-				break;
-			case XK_Escape:
-				suicide("you killed me! you faggot!");
-				break;
-			}
-			break;
-		}
 		case ButtonPress:
 			XGetWindowAttributes(dis, e.xbutton.subwindow, &atts);
 			s = e.xbutton;
 			break;
-		case MotionNotify:
-			// compress motion events to most recent one
-			while(XCheckTypedEvent(dis, MotionNotify, &e));
-			int dx = e.xbutton.x_root - s.x_root;
-			int dy = e.xbutton.y_root - s.y_root;
-			XMoveResizeWindow(dis, s.subwindow,
-							  atts.x + (s.button==1 ? dx : 0),
-							  atts.y + (s.button==1 ? dy : 0),
-							  max(1, atts.width + (s.button==3 ? dx : 0)),
-							  max(1, atts.height + (s.button==3 ? dy : 0)));
-			break;
 		case ButtonRelease:
 			s.subwindow = None;
-			break;
-		case MapRequest:
-			XSelectInput(dis, e.xmaprequest.window, EnterWindowMask);
-			XMapWindow(dis, e.xmaprequest.window);
 			break;
 		case EnterNotify: {
 			// compress enter notify events to mose recent one
@@ -141,8 +163,39 @@ int main(int argc, char *argv[])
 			XSetInputFocus(dis, v->window, RevertToPointerRoot, CurrentTime);
 			break;
 		}
+		case KeyPress: {
+			KeySym k = XLookupKeysym(&e.xkey, 0);
+
+			switch (k) {
+			case XK_r:
+				if (e.xkey.state & Mod1Mask) exec("rofi -show drun");
+				break;
+			case XK_Escape:
+				suicide("exiting!");
+				break;
+			}
+			break;
 		}
-		
+		case MapRequest:
+			XMapWindow(dis, e.xmaprequest.window);
+			break;
+		case MotionNotify:
+			// compress motion events to most recent one
+			while(XCheckTypedEvent(dis, MotionNotify, &e));
+			int dx = e.xbutton.x_root - s.x_root;
+			int dy = e.xbutton.y_root - s.y_root;
+			XMoveResizeWindow(dis, s.subwindow,
+							  atts.x + (s.button==1 ? dx : 0),
+							  atts.y + (s.button==1 ? dy : 0),
+							  max(1, atts.width + (s.button==3 ? dx : 0)),
+							  max(1, atts.height + (s.button==3 ? dy : 0)));
+			break;
+		default:
+#ifdef DEBUG
+			fprintf(stderr, "unhandled XEvent. type: %s\n", namexevent(e.type));
+#endif
+			break;
+		}
 	}
 
 	return 0;
