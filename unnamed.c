2915b58f1cd1553e2614855e4893cb2a7c609bcc
UnleashedFurai <67919489+UnleashedFurai@users.noreply.github.com>
2026-07-27 14:55:59 -0500

    add note to TODO comment

--- unnamed.c
+++ unnamed.c
@@ -7,6 +7,8 @@ TODO: handle floating windows
       is this really needed?
 TODO: there seems to be a bug when windows are deleted. need to move left/right
       multiple times sometimes, as if there is an invisible window
+	  UPD: seems to be because of windows that hide themselves while open
+	  like keepassxc
 */
 
 #include <X11/Xlib.h>
