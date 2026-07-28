cc8a2d8bef46e9ad34c4c26f86768f346e9ae9f1
UnleashedFurai <67919489+UnleashedFurai@users.noreply.github.com>
2026-07-28 16:07:02 -0500

    remove unneeded debug code

--- unnamed.c
+++ unnamed.c
@@ -84,54 +84,6 @@ int otherwmerr(Display *d, XErrorEvent *e)
 	exit(1);
 }
 
-#if 0
-const char* namexevent(int type)
-{
-    switch (type) {
-    case KeyPress:        return "KeyPress";
-    case KeyRelease:      return "KeyRelease";
-    case ButtonPress:     return "ButtonPress";
-    case ButtonRelease:   return "ButtonRelease";
-    case MotionNotify:    return "MotionNotify";
-    case EnterNotify:     return "EnterNotify";
-    case LeaveNotify:     return "LeaveNotify";
-    case FocusIn:         return "FocusIn";
-    case FocusOut:        return "FocusOut";
-	case KeymapNotify:    return "KeymapNotify";
-    case Expose:          return "Expose";
-	case GraphicsExpose:  return "GraphicsExpose";
-	case NoExpose:        return "NoExpose";
-	case VisibilityNotify:return "VisibilityNotify";
-	case CreateNotify:    return "CreateNotify";
-	case DestroyNotify:   return "DestroyNotify";
-    case UnmapNotify:     return "UnmapNotify";
-    case MapNotify:       return "MapNotify";
-	case MapRequest:      return "MapRequest";
-	case ReparentNotify:  return "ReparentNotify";
-	case ConfigureNotify: return "ConfigureNotify";
-	case ConfigureRequest:return "ConfigureRequest";
-	case GravityNotify:   return "GravityNotify";
-	case ResizeRequest:   return "ResizeRequest";
-	case CirculateNotify: return "CirculateNotify";
-	case CirculateRequest:return "CirculateRequest";
-	case PropertyNotify:  return "PropertyNotify";
-	case SelectionClear:  return "SelectionClear";
-	case SelectionRequest:return "SelectionRequest";
-	case SelectionNotify: return "SelectionNotify";
-	case ColormapNotify:  return "ColormapNotify";
-    case ClientMessage:   return "ClientMessage";
-	case MappingNotify:   return "MappingNotify";
-	case GenericEvent:    return "GenericEvent";
-	case LASTEvent:        return "LASTEvent";
-    default: {
-		static char buf[256];
-		snprintf(buf, sizeof(buf), "Unknown: %d", type);
-		return buf;	
-	}
-    }
-}
-#endif
-
 void checkwm(void)
 {
 	XSetErrorHandler(otherwmerr);
@@ -700,10 +652,6 @@ int main(int argc, char *argv[])
 		XNextEvent(dis, &e);
 		if (e.type >=0 && e.type < LASTEvent && handler[e.type])
 			handler[e.type](&e);
-#if 0
-		else
-			fprintf(stderr, "unhandled XEvent. type: %s\n", namexevent(e.type));
-#endif
 	}
 	return 0;
 }
